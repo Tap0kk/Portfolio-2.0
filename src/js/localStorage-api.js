24b6685352b9ce67abdb7b2394166eeee964f475
Deman <deman2805@gmail.com>
2024-11-27 17:36:03 +0200

    end header

--- src/js/localStorage-api.js
+++ src/js/localStorage-api.js
@@ -1,25 +1,25 @@
-// export function setDataToLocalStorage(key, value) {
-//     try {
-//       const normolizedState = JSON.stringify(value);
-//       localStorage.setItem(key, normolizedState);
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   }
+export function setDataToLocalStorage(key, value) {
+  try {
+    const normolizedState = JSON.stringify(value);
+    localStorage.setItem(key, normolizedState);
+  } catch (error) {
+    console.log(error);
+  }
+}
 
-//   export function getDataLocalStorage(key) {
-//     try {
-//       const normolizedState = localStorage.getItem(key);
-//       return normolizedState === null ? undefined : JSON.parse(normolizedState);
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   }
+export function getDataLocalStorage(key) {
+  try {
+    const normolizedState = localStorage.getItem(key);
+    return normolizedState === null ? undefined : JSON.parse(normolizedState);
+  } catch (error) {
+    console.log(error);
+  }
+}
 
-//   export function removeDataLocalStorage(key) {
-//     try {
-//       localStorage.removeItem(key);
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   }
+export function removeDataLocalStorage(key) {
+  try {
+    localStorage.removeItem(key);
+  } catch (error) {
+    console.log(error);
+  }
+}
